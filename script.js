f96d60ec2bc22958dda287b31df037222535ec61
Connor McGrew <connormcgrew003@gmail.com>
2023-03-09 22:24:42 -0700

    if statements

--- script.js
+++ script.js
@@ -3,8 +3,7 @@ function generatePassword() {
   if (passwordLength < 8 || passwordLength > 128) {
     alert("Password must be between 8 and 128 characters");
     return;
-  }
-} 
+  } 
 
 var upperCase = confirm("Click OK to confirm including uppercase characters");
  var lowerCase = confirm("Click OK to confirm including lowercase characters");
@@ -20,6 +19,24 @@ var upperCase = confirm("Click OK to confirm including uppercase characters");
  var lowerCaseSet = "abcdefghijklmnopqrstuvwxyz";
  var numbersSet = "0123456789";
  var specialCharactersSet = "!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~";
+ if (upperCase) {
+  characterTypes.push(upperCaseSet);
+}
+if (lowerCase) {
+  characterTypes.push(lowerCaseSet);
+}
+if (numbers) {
+  characterTypes.push(numbersSet);
+}
+if (specialCharacters) {
+  characterTypes.push(specialCharactersSet);
+}
+for (var i = 0; i < passwordLength; i++) {
+  var characterType = characterTypes[Math.floor(Math.random() * characterTypes.length)];
+  password += characterType[Math.floor(Math.random() * characterType.length)];
+}
+return password;
+}
 
 // // Assignment Code
 // var generateBtn = document.querySelector("#generate");
