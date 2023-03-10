624247e95528d705c3290c0e7e40716e82beb438
Connor McGrew <connormcgrew003@gmail.com>
2023-03-09 22:19:58 -0700

    generation password function

--- script.js
+++ script.js
@@ -1,14 +1,22 @@
-// Assignment Code
-var generateBtn = document.querySelector("#generate");
+function generatePassword() {
+  var passwordLength = prompt("How many characters would you like your password to contain?");
+  if (passwordLength < 8 || passwordLength > 128) {
+    alert("Password must be between 8 and 128 characters");
+    return;
+  }
+} 
 
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+// // Assignment Code
+// var generateBtn = document.querySelector("#generate");
 
-  passwordText.value = password;
+// // Write password to the #password input
+// function writePassword() {
+//   var password = generatePassword();
+//   var passwordText = document.querySelector("#password");
 
-}
+//   passwordText.value = password;
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+// }
+
+// // Add event listener to generate button
+// generateBtn.addEventListener("click", writePassword);
